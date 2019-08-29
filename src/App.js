c9bb102e29e3cbb0a73713e1cdaf9a8827c735a6
tejasgrd <tgarde@avaya.com>
2019-08-29 14:46:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React , {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state ={
+      string : 'Luke Skywalker'
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.string}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => this.setState({ string: 'Darth Vader'})}>Click to Change</button>
       </header>
     </div>
-  );
+    );
+  }
 }
-
 export default App;
