4a6535f481384664c3d92ab9221a1b64e70924fd
tejasgrd <tgarde@avaya.com>
2019-09-03 18:27:59 +0530

    SearchBox component with CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React , {Component} from 'react';
 import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component'
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,7 +11,7 @@ class App extends Component {
 
   constructor(){
     super();
-    let allMonsters = [];
+   
     this.state ={
       monsters : [],
       searchFiled : ''
@@ -20,27 +21,29 @@ class App extends Component {
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(users => {
-        this.setState({monsters : users});
-        this.allMonsters = users;
-        
-        });
+    .then(users => this.setState({monsters : users}) );
   }
+
+ 
 // All the Properties passed to components are received in props 
 // setState is aSync call , onChange is JSX
+// Tutorial Code
+// change of state with new SearchFiled causes component to rerender and hence 
+// render() methods get called which in turn created filterMonsters which then 
+// used by CardList Components 
+
   render() {
-    
+    const {monsters , searchFiled} = this.state;
+    const filterMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchFiled.toLocaleLowerCase())
+    );
     return (
       <div className="App">
-        <input type='search' placeholder='search monsters ' onChange={e => {
-          this.state.searchFiled = e.target.value;
-          
-          this.setState({monsters : this.allMonsters.filter(mon => mon.name.toLowerCase().includes(this.state.searchFiled.toLowerCase())),
-            searchFiled : this.state.searchFiled
-          })
-          console.log(this.state.searchFiled);
-        }} />
-        <CardList monsters={this.state.monsters}/>
+        <SearchBox 
+        placeholder='search monsters'
+        handleChange={e => this.setState({searchFiled : e.target.value})}
+         />
+        <CardList monsters={filterMonsters}/>
       
     </div>
     );
